71b100747bc58a4c3ea7a65e69541b92bc2878b6
Punnoose K Thomas <punnoose1997@gmail.com>
2026-03-03 22:58:25 +0100

    Class functions to construct search URL

---
+++ Backend/live_search/searchUrlBuilder.py
@@ -0,0 +1,32 @@
+class SearchUrlBuilderByKeywords:
+  base_url: str
+  def __init__(self, url:str):
+    self.base_url = url
+
+  def build_url(self, keywords:list[str], country:str, selector:str):
+    if selector == 'free-patents-online':
+      return self.free_patents_online(keywords)
+    if selector == 'google-patents':
+      return self.google_patents(keywords, country)
+    return None
+
+  def free_patents_online(self, keywords:list[str]):
+    sort_param = 'sort=relevance'
+    keywords_merged = ''
+
+    for entry in keywords:
+      entry.replace(' ', '+')
+
+    keywords_merged = keywords.join('+')
+    return f'{self.base_url}?{sort_param}&query_txt={keywords_merged}'
+
+  def google_patents(self, keywords:list[str]):
+    for word in keywords:
+      word.replace(' ', '+')
+    keywords_merged = keywords.join('+')
+
+    q_value = f"q=({keywords_merged})"
+    oq_value = f"oq={keywords_merged}"
+    page_limit = 'num=5000'
+
+    return f'{self.base_url}/?{q_value}&{page_limit}&{oq_value}'
